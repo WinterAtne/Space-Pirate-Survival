1f0536999347e782d2863d9a7a43cf4a5d68a38b
WinterAtne <Winter54b@gmail.com>
2024-02-26 23:50:09 -0700

    Made levels work

--- scenes/main.gd
+++ scenes/main.gd
@@ -1,6 +1,14 @@
 extends Node2D
 
+@export var level : PackedScene
+
+var current_level : Node2D
+
 func _on_start_button_down():
 	%MainMenu.visible = false
 	%Background.set_process(false)
+	
+	current_level = level.instantiate()
+	add_child(current_level)
+	
 	
