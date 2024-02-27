2674b09afaf1f1408ec604c51bede7f5eb05a8ec
WinterAtne <Winter54b@gmail.com>
2024-02-27 00:03:39 -0700

    Reload level on player death

--- scenes/main.gd
+++ scenes/main.gd
@@ -2,13 +2,29 @@ extends Node2D
 
 @export var level : PackedScene
 
-var current_level : Node2D
+var current_level : Level
 
 func _on_start_button_down():
 	%MainMenu.visible = false
 	%Background.set_process(false)
 	
-	current_level = level.instantiate()
-	add_child(current_level)
+	load_level(level)
 	
+
+#this seemingly useless function is here in case we add more than one level
+#or a death screen or something
+func restart_level() -> void:
+	load_level(level)
+	
+
+func load_level(new_level : PackedScene) -> void:
+	if current_level:
+		current_level.queue_free()
+		#the level is pretty big so we should wait a hot sec while it gets deleted
+		await get_tree().process_frame
+		
+	
+	current_level = new_level.instantiate()
+	add_child(current_level)
+	current_level.level_ended.connect(restart_level.bind())
 	
